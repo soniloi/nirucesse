10e4ceb36f3f1868dc5781e28fab3ab8c4861ea1
soniloi <niloinso@tcd.ie>
2016-06-26 00:51:19 +0100

    Enabled text wrapping on output; removed write_tabbed

--- src/terminal.rs
+++ src/terminal.rs
@@ -6,36 +6,38 @@ const PROMPT_END: &'static str = " > ";
 const PROMPT_FULL: &'static str = "---------> ";
 const PROMPT_TAB: &'static str = "         > ";
 const PROMPT_WIDTH: usize = 11;
+const CONSOLE_EFFECTIVE_WIDTH: usize = CONSOLE_WIDTH - PROMPT_WIDTH;
 
 pub fn write_full(st: &str) {
 	let raw: Vec<char> = st.chars().collect();
-	let bound = CONSOLE_WIDTH - PROMPT_WIDTH;
-	if raw.len() < bound {
-		write_prompted(st, PROMPT_FULL);
+	write_sections(raw, 0, PROMPT_FULL);
+}
+
+// Write the next section to the terminal; a section is the characters from start_index up to
+// the next newline (if present) or the last whitespace character before the effective console
+// width is reached; returns only when a section shorter than the effective width has been
+// printed
+fn write_sections(chars: Vec<char>, start_index: usize, prompt: &str) {
+	let remaining = chars.len() - start_index;
+	if remaining < CONSOLE_EFFECTIVE_WIDTH {
+		let content: String = to_str(&chars[(start_index as usize)..]);
+		write_prompted(&content, prompt);
 		return;
 	}
 
-	let mut start_index: i32 = 0;
-	let mut stop_index: i32 = get_newline_index_within_width(&raw[(start_index as usize)..bound]);
+	let max_index = start_index + CONSOLE_EFFECTIVE_WIDTH;
+
+	let mut stop_index: i32 = get_newline_index_within_width(&chars[(start_index as usize)..max_index]);
 	if stop_index == -1 {
-		stop_index = get_last_space_index_within_width(&raw[(start_index as usize)..bound]);
+		stop_index = start_index as i32 + get_last_space_index_within_width(&chars[(start_index as usize)..max_index]);
 	}
 	if stop_index == -1 {
-		stop_index = raw.len() as i32;
+		stop_index = chars.len() as i32;
 	}
+	let content: String = to_str(&chars[start_index..(stop_index as usize)]);
+	write_prompted(&content, prompt);
 
-	let mut content: String = to_str(&raw[0..(stop_index as usize)]);
-	write_prompted(&content, PROMPT_FULL);
-
-	start_index = stop_index + 1;
-	content = to_str(&raw[(start_index as usize)..]);
-	write_prompted(&content, PROMPT_TAB);
-}
-
-// Write to console using a 'tabbed' (empty) prompt
-// TODO: this is public for now, but possibly should be made private later, or nemoved entirely
-pub fn write_tabbed(st: &str) {
-	write_prompted(st, PROMPT_TAB);
+	write_sections(chars, stop_index as usize + 1, PROMPT_TAB);
 }
 
 // Write to console with a given prompt
@@ -70,10 +72,10 @@ fn get_newline_index_within_width(chs: &[char]) -> i32 {
 // Return the index of the last space within a string slice
 // If no space character found, return -1
 fn get_last_space_index_within_width(chs: &[char]) -> i32 {
-	let mut i: usize = chs.len() - 1;
+	let mut i: i32 = chs.len() as i32 - 1;
 	while i >= 0 {
-		if chs[i] == ' ' {
-			return i as i32;
+		if chs[i as usize] == ' ' {
+			return i;
 		}
 		i -= 1;
 	}
