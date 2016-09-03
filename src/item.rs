c6c28627cacbbde897b8fb114eeed906941d009e
soniloi <niloinso@tcd.ie>
2016-09-03 12:28:40 +0100

    Added treasure property; updated how treasures/obstructions displayed

--- src/item.rs
+++ src/item.rs
@@ -7,6 +7,7 @@ const CTRL_ITEM_FRAGILE: u32 = 0x200; // Whether the item would survive throwing
 const CTRL_ITEM_WEARABLE: u32 = 0x400; // Whether the item is to be worn by the player rather than carried
 const CTRL_ITEM_ESSENTIAL: u32 = 0x1000; // Whether the item is essential to basic gameplay
 const CTRL_ITEM_EDIBLE: u32 = 0x2000; // Whether the item is any sort of food or drink
+const CTRL_ITEM_TREASURE: u32 = 0x8000; // Whether the item is a treasure
 const CTRL_ITEM_RECIPIENT: u32 = 0x80000; // Whether the item may be a recipient (i.e. of gifts or food)
 
 pub struct Item {
@@ -111,6 +112,10 @@ impl Item {
 		self.has_property(CTRL_ITEM_RECIPIENT)
 	}
 
+	fn is_treasure(&self) -> bool {
+		self.has_property(CTRL_ITEM_TREASURE)
+	}
+
 	fn get_switch_status(&self) -> String {
 		String::from("currently ") + if self.on {"on"} else {"off"}
 	}
@@ -134,7 +139,9 @@ impl Item {
 		if self.is_switchable() {
 			result = result + " (" + &self.get_switch_status() + ")"
 		}
-		result + " here."
+		result = result + " here";
+		result = result + if self.is_obstruction() || self.is_treasure() {"!"} else {"."};
+		result
 	}
 
 	pub fn get_size(&self) -> u32 {
